5b89d18b4b4d8bed4a7598d03f6a2e822bb697d6
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-01-30 09:07:54 -0600

    Created a few Game class methods, need to do a few more and then incorporate them into app.rb.

--- 2015-01-29-game-class.rb
+++ 2015-01-29-game-class.rb
@@ -7,47 +7,54 @@ require "2015-01-29-rps-player-class.rb"
 
 
 class Game
-  def initialize 
+  def initialize(move1, move2)
+    @move1 = move1
+    @move2 = move2
   end
   
   
-  def judge_moves
-    
-    #need this?
+  def are_moves_acceptable
+    if ((@move2 != "rock" && @move2 != "scissors") && @move2 != "paper") && ((@move1 != "rock" && @move1 != "scissors") && @move1 != "paper")
+      print "You're both hopeless. EVERYONE LOSES. (Except for me.)"
+
+    elsif (@move1 != "rock" && @move1 != "scissors") && @move1 != "paper"
+      print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{@move1}\" nonsense. Player 2 wins by default."
+
+    elsif (@move2 != "rock" && @move2 != "scissors") && @move2 != "paper"
+      print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{@move2}\" nonsense. Player 1 wins by default."
+    else 
+  end
   
+  def who_wins
+  end
+  
+  def print_rules
   end
 
   #REALLY NOT SURE WHAT METHODS TO DO NOW ... DO I NEED TO MAKE SOME OF THIS INTO METHODS HERE?
   
-  # if move_1 == move_2
+  # if @move1 == @move2
   #   print "It's a tie!"
   #
-  # elsif move_1 == "rock" && move_2 == "paper"
+  # elsif @move1 == "rock" && @move2 == "paper"
   #   print "Player 2 wins!"
   #
-  # elsif move_1 == "rock" && move_2 == "scissors"
+  # elsif @move1 == "rock" && @move2 == "scissors"
   #   print "Player 1 wins!"
   #
-  # elsif move_1 == "paper" && move_2 == "rock"
+  # elsif @move1 == "paper" && @move2 == "rock"
   #   print "Player 1 wins!"
   #
-  # elsif move_1 == "paper" && move_2 == "scissors"
+  # elsif @move1 == "paper" && @move2 == "scissors"
   #   print "Player 2 wins!"
   #
-  # elsif move_1 == "scissors" && move_2 == "paper"
+  # elsif @move1 == "scissors" && @move2 == "paper"
   #   print "Player 1 wins!"
   #
-  # elsif move_1 == "scissors" && move_2 == "rock"
+  # elsif @move1 == "scissors" && @move2 == "rock"
   #   print "Player 2 wins!"
   #
-  # elsif  ((move_2 != "rock" && move_2 != "scissors") && move_2 != "paper") && ((move_1 != "rock" && move_1 != "scissors") && move_1 != "paper")
-  #   print "You're both hopeless. EVERYONE LOSES. (Except for me.)"
-  #
-  # elsif (move_1 != "rock" && move_1 != "scissors") && move_1 != "paper"
-  #   print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{move_1}\" nonsense. Player 2 wins by default."
-  #
-  # elsif (move_2 != "rock" && move_2 != "scissors") && move_2 != "paper"
-  #   print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{move_2}\" nonsense. Player 1 wins by default."
+
   #
   # else
   #   print "Something went wrong ... we're sending the space monkeys into the computer to try and fix it."
--- app.rb
+++ app.rb
@@ -3,21 +3,26 @@ require_relative "2015-01-29-rps-player-class.rb"
 require_relative "2015-01-29-game-class.rb"
 
 
-# Driver - program that runs methods with limited input from players. 
-
-puts "Player1, what's your name?"
+puts "Player 1, what's your name?"
 name1 = gets.chomp.to_sym
 
-puts "Player2, what's your name?"
+puts "Player 2, what's your name?"
 name2 = gets.chomp.to_sym
 
-# Theorectically, this will now pass to the arguments for the instantiation of the class and will thus populate the hashes for each player. 
 
 player1 = Player.new(name1)
 player2 = Player.new(name2)
 
 
+puts "Player1, what's your move?"
+move1 = gets.chomp.downcase
 
+puts "Player2, what's your move?"
+move2 = gets.chomp.downcase
+
+game = Game.new(move1, move2)
+
+game.are_moves_acceptable
 
 
 # if move_1 == move_2
@@ -41,14 +46,7 @@ player2 = Player.new(name2)
 # elsif move_1 == "scissors" && move_2 == "rock"
 #   print "Player 2 wins!"
 #
-# elsif  ((move_2 != "rock" && move_2 != "scissors") && move_2 != "paper") && ((move_1 != "rock" && move_1 != "scissors") && move_1 != "paper")
-#   print "You're both hopeless. EVERYONE LOSES. (Except for me.)"
-#
-# elsif (move_1 != "rock" && move_1 != "scissors") && move_1 != "paper"
-#   print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{move_1}\" nonsense. Player 2 wins by default."
-#
-# elsif (move_2 != "rock" && move_2 != "scissors") && move_2 != "paper"
-#   print "Um, your only options are P-A-P-E-R, S-C-I-S-S-O-R-S or R-O-C-K. None of this \"#{move_2}\" nonsense. Player 1 wins by default."
+
 #
 # else
 #   print "Something went wrong ... we're sending the space monkeys into the computer to try and fix it."
